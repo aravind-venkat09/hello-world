13ab77078c68204dbd6ba9c07ac8006d6d88e2c4
Aravind Venkat <aravkat@gmail.com>
2023-05-18 20:44:01 -0500

    Updated JS to get word count from the website

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ function handleSubmit(event) {
     event.preventDefault(); // Prevents the page from refreshing
     const textInput = document.getElementById("textInput").value;
     const outputDiv = document.getElementById("outputDiv");
-    outputDiv.textContent = `You entered: ${textInput}`;
+    const wordCount = document.getElementById("wordCount");
+  
+    // Count the number of words
+    const words = textInput.split(" ");
+    const count = words.length;
+  
+    // Display the word count
+    wordCount.textContent = `Word count: ${count}`;
+  
+    // Clear the input field
+    document.getElementById("textInput").value = "";
   }
   
   // Add event listener to the form
   const form = document.getElementById("textForm");
   form.addEventListener("submit", handleSubmit);
+  
   
